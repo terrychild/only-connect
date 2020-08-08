5aa41cf9e20783791f71a8da6b9a5483a4426214
Terry Child <tc@moohar.com>
2020-08-08 15:54:30 +0100

    Set up grid and test sizing

--- www/only-connect.js
+++ www/only-connect.js
@@ -1,5 +1,23 @@
 "use strict";
 
 (function() {
+	function html(parent, tag, cssClass) {
+		let child = parent.appendChild(document.createElement(tag));
+		if(cssClass) {
+			child.classList.add(cssClass);
+		}
+		return child;
+	}
 
+	function createWall() {
+		let box = html(html(document.querySelector("body"), "div", "box-16x9"), "div", "box-inner");
+
+		let wall = html(box, "div", "wall");
+		let bricks = [];
+		for(let i=0; i<16; i++) {
+			bricks.push(html(wall, "div"));
+		}
+	}
+
+	createWall();
 })();
